8be6c20cfa0576b96cceb410b4fc046a0fc2be2e
Mr Revolution <DIDOXWARRIOR@GMAIL.COM>
2026-05-25 19:55:37 +0200

    Fix formatting of Firebase configuration keys

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Configuration Firebase (Remplace avec tes propres clés de projet Firebase)
 const firebaseConfig = {
-    apiKey: " AIzaSyDzi4YFVsVOiCzylmuHwi_kmM0vnlFNMuM ",
-    authDomain: " portfolio-cms-2b5ef.firebaseapp.com",
-    projectId: " portfolio-cms-2b5ef ",
-    storageBucket: "portfolio-cms-2b5ef.firebasestorage.app ",
+    apiKey: "AIzaSyDzi4YFVsVOiczylmuHwi_kmM0vnlFNMuM",
+    authDomain: "portfolio-cms-2b5ef.firebaseapp.com",
+    projectId: "portfolio-cms-2b5ef",
+    storageBucket: "portfolio-cms-2b5ef.firebasestorage.app",
     messagingSenderId: "618275140022",
-    appId: "1:618275140022 :web :976b714748fb412c9d9117"
+    appId: "1:618275140022:web:976b714748fb412c9d9117"
 };
 
 // Initialisation de Firebase via CDN (importé dans les fichiers HTML)
